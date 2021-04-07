56acd3b19f8ebedba75c6b866ce9c960fc18637e
Daniel Pitts <danielmpitts@gmail.com>
2021-04-07 14:29:47 -0700

    Initial Commit

---
+++ JournalsTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JournalsTests
+//
+//  Created by Daniel Pitts on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
