7c1f4c7116cc5b03622e7fdbc3896b744d609b7d
Daniel Pitts <danielmpitts@gmail.com>
2021-08-02 13:08:13 -0500

    Slight formatting changes to "Getting Started" pages

--- LittleJournals/ViewControllers/InfoViewController.swift
+++ LittleJournals/ViewControllers/InfoViewController.swift
@@ -11,6 +11,11 @@ class InfoViewController: UIViewController {
    
 //    let bgColor = UIColor(red: 3/255, green: 110/255, blue: 125/255, alpha: 1.0)
     
+    @IBOutlet var journalsScreenImageView: UIImageView!
+    @IBOutlet var entriesScreenImageView: UIImageView!
+    @IBOutlet var pagesScreenImageView: UIImageView!
+    @IBOutlet var gridScreenImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,16 @@ class InfoViewController: UIViewController {
 //        navigationController?.navigationBar.backgroundColor = bgColor
         let dismissButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(dismissView))
         navigationItem.rightBarButtonItem = dismissButton
+        
+//        journalsScreenImageView.layer.borderWidth = 1
+//        journalsScreenImageView.layer.borderColor = CGColor(gray: 0.8, alpha: 1.0)
+//        entriesScreenImageView.layer.borderWidth = 1
+//        entriesScreenImageView.layer.borderColor = CGColor(gray: 0.8, alpha: 1.0)
+//        pagesScreenImageView.layer.borderWidth = 1
+//        pagesScreenImageView.layer.borderColor = CGColor(gray: 0.8, alpha: 1.0)
+//        gridScreenImageView.layer.borderWidth = 1
+//        gridScreenImageView.layer.borderColor = CGColor(gray: 0.8, alpha: 1.0)
+        
     }
     
     @objc func dismissView() {
